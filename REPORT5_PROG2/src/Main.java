1764ea76283755c3849602c4c655bc644bbef5f6
Issei Nagase <e235732@ie.u-ryukyu.ac.jp>
2023-12-11 17:31:39 +0900

    ステップ3

--- REPORT5_PROG2/src/Main.java
+++ REPORT5_PROG2/src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // 条件1: String型変数 str を宣言し、初期値として null を代入する
-        String str = null;
+        // 条件1: String型の変数strに、文字列"壱百満"を代入する
+        String str = "壱百満";
 
-        // 条件2と条件3: NullPointerException を catch して処理する
+        // 条件2: Integer.parseInt(String s)メソッドを用いて、int型変数valueに代入する
         try {
-            // NullPointerExceptionが発生するコード
-            int length = str.length();
-            System.out.println("Stringの長さ: " + length);
-        } catch (NullPointerException e) {
-            // 例外が発生したことを標準出力
-            System.out.println("NullPointerExceptionが発生しました");
-
-            // 例外インスタンスを用いて、getMessage()を出力
-            System.out.println("例外メッセージ: " + e.getMessage());
+            int value = Integer.parseInt(str);
+            System.out.println("変換後の値: " + value);
+        } catch (NumberFormatException e) {
+            // 数字に変換できない場合の例外処理
+            System.out.println("NumberFormatExceptionが発生しました: " + e.getMessage());
         }
     }
 }
