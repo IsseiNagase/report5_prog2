98091efabfd41d47e953a4929248346ee68e546d
Issei Nagase <e235732@ie.u-ryukyu.ac.jp>
2023-12-11 17:20:25 +0900

    ステップ2

--- REPORT5_PROG2/src/Main.java
+++ REPORT5_PROG2/src/Main.java
@@ -3,14 +3,17 @@ public class Main {
         // 条件1: String型変数 str を宣言し、初期値として null を代入する
         String str = null;
 
-        // 条件2: str.length() の内容を表示しようとする
+        // 条件2と条件3: NullPointerException を catch して処理する
         try {
-            // 条件2を満たすコード（NullPointerExceptionが発生する）
+            // NullPointerExceptionが発生するコード
             int length = str.length();
             System.out.println("Stringの長さ: " + length);
         } catch (NullPointerException e) {
-            // 例外が発生した場合の処理
-            System.out.println("例外が発生しました: " + e.getMessage());
+            // 例外が発生したことを標準出力
+            System.out.println("NullPointerExceptionが発生しました");
+
+            // 例外インスタンスを用いて、getMessage()を出力
+            System.out.println("例外メッセージ: " + e.getMessage());
         }
     }
 }
